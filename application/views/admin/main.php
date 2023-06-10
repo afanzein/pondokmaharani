76813b71b481ae58adf5990e42b93a23ab78869b
afanzein <afanaz99@gmail.com>
2023-06-10 22:41:04 +0800

    Initial commit

---
+++ application/views/admin/main.php
@@ -0,0 +1,71 @@
+<?php
+  //==================================== HOME ====================================
+  if ($page == 'home') {
+  ?>
+  <div class="content-wrapper">
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1><?php echo  $judul; ?></h1>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-6 col-12">
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3><?php echo $jml_material; ?></h3>
+
+                <p>Jumlah material</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-cube"></i>
+              </div>
+              <a href="<?php echo base_url('dashboard/material') ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          
+        </div>
+      </div>
+    </section>
+
+    <section class="content">
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Selamat Datang Admin</h3>
+          <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+              <i class="fas fa-minus"></i>
+            </button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+              <i class="fas fa-times"></i>
+            </button>
+          </div>
+        </div>
+
+        <div class="card-body">
+          <h2>Info</h2>
+          <p>Selamat datang di halaman admin khusus web hotel kami! Halaman ini dirancang khusus 
+            untuk para admin dengan tujuan memberikan akses dan kontrol penuh dalam mengelola berbagai 
+            aspek operasional hotel. Anda dapat dengan mudah mengatur reservasi, ketersediaan kamar, 
+            informasi tamu, harga, promosi, laporan keuangan, dan statistik penting lainnya. 
+            <br>
+            Terima kasih telah menjadi bagian dari tim admin hotel kami, dan selamat bekerja!</p>
+          <p></p>
+
+        </div>
+        <div class="card-footer">
+          Create @2022
+        </div>
+      </div>
+
+    </section>
+  </div>
+<?php
+}
