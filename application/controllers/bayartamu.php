e170166fc48acfb7caa83478b077dfd7260e7903
afanzein <afanaz99@gmail.com>
2023-08-01 01:39:32 +0800

    Update bayartamu.php

--- application/controllers/bayartamu.php
+++ application/controllers/bayartamu.php
@@ -4,6 +4,11 @@ class Bayartamu extends CI_Controller{
     public function index($id_pemesanan){
         $this->load->model('M_pembayaran');
         $id_akun = $this->session->userdata('id_akun');
+            // Retrieve the id_pemesanan from the URL query parameter
+        $id_pemesanan = $this->input->get('id_pemesanan');
+
+        // Use the id_pemesanan as needed, for example, pass it to the view
+        $data['id_pemesanan'] = $id_pemesanan;
         $data['title'] = 'Halaman Pembayaran';
         $data['pembayaran'] = $this->M_pembayaran->get_pembayaran_data($id_akun, $id_pemesanan);
 
