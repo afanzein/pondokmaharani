3f4932067bc8e2401c724c725308de8f28bcf107
afanzein <afanaz99@gmail.com>
2023-08-06 11:10:24 +0800

    Update reservation.php

--- application/views/user/reservation.php
+++ application/views/user/reservation.php
@@ -138,45 +138,56 @@ if ($this->session->userdata('id_akun')) {
 <script>
 
   //slideshow
+  document.addEventListener("DOMContentLoaded", function () {
+  // Initialize the slide index
   let slideIndex = 1;
 
-function showSlides(n) {
-  let slides = document.getElementsByClassName("mySlides");
+  function showSlides(n) {
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+  }
+
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+
+  function currentSlide(n) {
+    showSlides((slideIndex = n));
+  }
+
+  // Auto-start the slideshow
+  showSlides(slideIndex);
+
+  // Add event listeners to the prev and next buttons
+  document.querySelector(".prev").addEventListener("click", function () {
+    plusSlides(-1);
+  });
+
+  document.querySelector(".next").addEventListener("click", function () {
+    plusSlides(1);
+  });
+
+  // Add event listeners to the dot elements
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
   for (let i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].addEventListener("click", function () {
+      currentSlide(i + 1);
+    });
   }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
-
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-// Auto-start the slideshow
-showSlides(slideIndex);
-
-// Add event listeners to the prev and next buttons
-document.querySelector(".prev").addEventListener("click", function () {
-  plusSlides(-1);
-});
-
-document.querySelector(".next").addEventListener("click", function () {
-  plusSlides(1);
 });
   
 //Hitung bayar
