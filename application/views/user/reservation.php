404581818aed1270aef1cea1d83622f4cb99d7c8
afanzein <afanaz99@gmail.com>
2023-08-06 12:29:28 +0800

    Update reservation.php
    remove onclick on next and prev button

--- application/views/user/reservation.php
+++ application/views/user/reservation.php
@@ -47,8 +47,8 @@ if ($this->session->userdata('id_akun')) {
         
             
             <!-- Next and previous buttons -->
-            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+            <a class="prev" >&#10094;</a>
+            <a class="next" >&#10095;</a>
 
             <!-- Thumbnail images -->
             <div class="row">
